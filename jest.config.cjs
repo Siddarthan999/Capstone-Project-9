c38ee77cf312ff4f58e6bd0766e0b48580d9a4dc
Siddarthan999 <siddarthan999@gmail.comm>
2024-10-14 14:52:51 +0530

    Code Quality and Testing

--- jest.config.cjs
+++ jest.config.cjs
@@ -5,7 +5,7 @@ module.exports = {
     '^.+\\.jsx?$': 'babel-jest',
   },
   moduleFileExtensions: ['js', 'jsx', 'json', 'node'],
-  setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'], // Make sure the path is correct
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
   moduleNameMapper: {
     '\\.(css|less|scss|sass)$': '<rootDir>/src/__mocks__/styleMock.js',
   },
